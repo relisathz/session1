379935f5263711f697f5e51313c14ae8bc8d3aca
relisathz <dula98sathsarani@gmail.com>
2025-02-08 17:23:08 +0530

    4,5 session
    code

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Button from "./Button";
+import Hero from "./Hero";
+import Navigation from "./Navigation";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const name = "Manupa";
+  const cartCount = 2;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Navigation name={name} cartCount={cartCount} />
+      <Hero />
+      <div className="p-4">
+        <Button>Buy Now</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
